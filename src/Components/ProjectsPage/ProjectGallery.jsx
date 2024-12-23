d41c2a108f6038e2a2c52e4ed66fc5456f6c9438
Ian Kernohan <kernohani1@gmail.com>
2024-12-23 16:44:27 -0500

    added image gallery

--- src/Components/ProjectsPage/ProjectGallery.jsx
+++ src/Components/ProjectsPage/ProjectGallery.jsx
@@ -1,28 +1,19 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
+import { Fullscreen, Zoom } from "yet-another-react-lightbox/plugins";
+import Lightbox from "yet-another-react-lightbox";
+import images from "./images";
+import "yet-another-react-lightbox/styles.css";
 
 export default function ProjectGallery() {
-  const images = {
-    bathroom: [
-      "bathroom/IMG_4343.jpg",
-      "bathroom/IMG_4344.jpg",
-      "bathroom/IMG_4345.jpg",
-      "bathroom/IMG_4510.jpg",
-      "bathroom2/IMG_4099.jpg",
-      "bathroom2/IMG_4100.jpg",
-      "bathroom2/IMG_4101.jpg",
-      "bathroom2/IMG_4102.jpg",
-      "bathroom2/IMG_4103.jpg",
-      "bathroom3/IMG_4397.jpg",
-      "bathroom3/IMG_4398.jpg",
-      "bathroom3/IMG_4400.jpg",
-      "bathroom3/IMG_4402.jpg",
-    ],
-    garage: ["garage/IMG_4186.jpg"],
-    pavillion: ["pavillion/Attachment_1.JPG"],
-    shower: ["shower/IMG_3811.jpg"],
-  };
-
   const { name } = useParams();
+  const [isOpen, setIsOpen] = useState(false);
+  const [currIndex, setCurrIndex] = useState(0);
+
+  function openLightBox(index) {
+    setIsOpen(true);
+    setCurrIndex(index);
+  }
 
   const upperCaseTitle =
     String(name).charAt(0).toUpperCase() + String(name).slice(1);
@@ -33,9 +24,31 @@ export default function ProjectGallery() {
       <section>
         {images[name].map((img, idx) => (
           <div key={idx} className="image-container">
-            <img src={`/images/${img}`} />
+            <img
+              src={`${img.src}`}
+              alt={`${name} image`}
+              onClick={() => openLightBox(idx)}
+            />
           </div>
         ))}
+
+        {isOpen && (
+          <Lightbox
+            plugins={[Zoom, Fullscreen]}
+            zoom={{ maxZoomPixelRatio: 5, defaultZoom: 3 }}
+            open={isOpen}
+            close={() => setIsOpen(false)}
+            slides={images[name]}
+            index={currIndex}
+            onPrev={() =>
+              setCurrIndex(
+                (currIndex + images[name].length - 1) % images[name].length
+              )
+            }
+            onNext={() => setCurrIndex((currIndex + 1) % images[name].length)}
+            carousel={{ imageFit: "cover" }}
+          />
+        )}
       </section>
     </div>
   );
---
+++ src/Components/ProjectsPage/images.js
@@ -0,0 +1,36 @@
+const images = {
+  bathroom: [
+    { src: "/images/bathroom/IMG_4343.jpg", fill: true },
+    { src: "/images/bathroom/IMG_4344.jpg", fill: true },
+    { src: "/images/bathroom/IMG_4345.jpg", fill: true },
+    { src: "/images/bathroom/IMG_4510.jpg", fill: true },
+    { src: "/images/bathroom2/IMG_4099.jpg", fill: true },
+    { src: "/images/bathroom2/IMG_4100.jpg", fill: true },
+    { src: "/images/bathroom2/IMG_4101.jpg", fill: true },
+    { src: "/images/bathroom2/IMG_4102.jpg", fill: true },
+    { src: "/images/bathroom2/IMG_4103.jpg", fill: true },
+    { src: "/images/bathroom3/IMG_4397.jpg", fill: true },
+    { src: "/images/bathroom3/IMG_4398.jpg", fill: true },
+    { src: "/images/bathroom3/IMG_4400.jpg", fill: true },
+    { src: "/images/bathroom3/IMG_4402.jpg", fill: true },
+  ],
+  garage: [
+    { src: "/images/garage/IMG_4186.jpg", fill: true },
+    { src: "/images/garage/IMG_4188.jpg", fill: true },
+    { src: "/images/garage/IMG_4217.jpg", fill: true },
+    { src: "/images/garage/IMG_4357.jpg", fill: true },
+  ],
+  pavillion: [
+    { src: "/images/pavillion/IMG_3909.jpg", fill: true },
+    { src: "/images/pavillion/IMG_3910.jpg", fill: true },
+    { src: "/images/pavillion/Attachment_1.JPG", fill: true },
+    { src: "/images/pavillion/Attachment.JPG", fill: true },
+  ],
+  shower: [
+    { src: "/images/shower/IMG_3811.jpg", fill: true },
+    { src: "/images/shower/IMG_3812.jpg", fill: true },
+    { src: "/images/shower/IMG_3813.jpg", fill: true },
+  ],
+};
+
+export default images;
